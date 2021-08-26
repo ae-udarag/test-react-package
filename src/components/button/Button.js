1cfb7a369a3eed40c6e179ac98ca676374313f04
G.D Udara Lahiru Sampath <udara.g@aeturnum.com>
2021-08-26 11:38:26 +0530

    update config files

---
+++ src/components/button/Button.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export const CustomButton = ({ text }) => {
+    return (
+        <div style={{ width: "100%", margin: "0px", padding: "0px" }}>
+            <span>{text}</span>
+        </div>
+    );
+}
